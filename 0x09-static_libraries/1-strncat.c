ffbff672b6e57e087d3842e337d4adb66d45c31e
timan01 <emmanuel.ighovwemerehor@eng.uniben.edu>
2023-03-13 03:55:20 -0700

    My work on libraries

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates 2 string
+ * @dest: the destination of the string
+ * @src: the source of the string
+ * @n: the length of the int
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,3 @@
+I am creating a static library libmy.a, containing all the functions listed in the main header file
+
+Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory
