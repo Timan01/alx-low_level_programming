382c031419c4a2f35992250b264658e8012f2aeb
timan01 <emmanuel.ighovwemerehor@eng.uniben.edu>
2023-04-10 11:00:56 -0700

    bit manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,12 +9,11 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int mask;
+	unsigned long int mask = 1ul << index;
 
-	if (index >= (sizeof(*n) * 8)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	mask = 1UL << index;
 	*n |= mask;
 
 	return (1);
